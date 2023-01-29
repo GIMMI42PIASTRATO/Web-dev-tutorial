d6fce3c870ddc33ac911fd3f3f2f79d34db6cf4c
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2023-01-29 15:53:09 +0100

    feat(Javascript, css, html): finished the nasa api website

--- apiTutorial/nasaApi/script.js
+++ apiTutorial/nasaApi/script.js
@@ -1,9 +1,57 @@
-// const image = document.getElementById('imageContainer')
+const image = document.getElementById('imageContainer')
+const hideElement = document.getElementById('error')
+const author = document.getElementById('author')
+const explanation = document.getElementById('explanation')
 
-fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-01-01&end_date=2023-01-07&api_key=u9i29g1nvwwld876jukZihriAFMdsRM1GBbGKiiO')
-.then(response => response.json())
-.then(data => {
-    console.log(data.near_earth_objects['2023-01-01'])
-    // image.innerHTML = `<img id='image' src='${data.hdurl}'></img>`
+const date = new Date()
+const year = date.getFullYear()
+const month = date.getMonth() + 1
+const day = date.getDate()
+
+let fullDate = String(year + '-' + month + '-' + day)
+console.log(fullDate)
+
+const inputDate = document.getElementById('input')
+console.log(inputDate)
+
+inputDate.addEventListener('keyup', event => {
+    inputDate.value = ''
+    hideElement.style.display = 'contents'
+    hideElement.innerHTML = "*It's only possible to use the day picker."
+    setTimeout(() => {
+        hideElement.style.display = 'none'
+    }, 2000);
 })
+
+inputDate.addEventListener('change', event => {
+    console.log(inputDate.value)
+    fullDate = inputDate.value
+    console.log("🚀 ~ file: script.js:16 ~ fullDate", fullDate)
+
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=hf6m2GMeUwBgDtwlNXcP9Ywov9k4dOhihu7IpfeL&date=${fullDate}`)
+    .then(response => response.json())
+    .then(data => {        
+        console.log(data)
+        if (data.code == 400) {
+            image.innerHTML = `<div style='color: red; font-weight: bold;'>*${data.msg}</div>`
+            return
+        }
+        image.innerHTML = `<img id='image' src='${data.url}'></img>`
+
+        author.innerHTML = data.copyright
+
+        explanation.innerHTML = data.explanation
+    })
+})
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=hf6m2GMeUwBgDtwlNXcP9Ywov9k4dOhihu7IpfeL&date=${fullDate}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        image.innerHTML = `<img id='image' src='${data.url}'></img>`
+
+        author.innerHTML = data.copyright
+
+        explanation.innerHTML = data.explanation
+    })
 
